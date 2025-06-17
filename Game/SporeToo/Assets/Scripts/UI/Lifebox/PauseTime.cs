057af1c420c0a2ac8b335f6c789cd4cbc3a2b1d6
Gabriel Rios <gabrielrios763@gmail.com>
2025-06-17 18:48:56 -0300

    Pause and Play Button

---
+++ Game/SporeToo/Assets/Scripts/UI/Lifebox/PauseTime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor.Analytics;
+using UnityEngine;
+
+namespace UI.Lifebox
+
+{
+    public class PauseTime : MonoBehaviour
+    {
+
+        public GameObject PauseTimeTick;
+        
+        public void PauseGame()
+        {
+            PauseTimeTick.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        public void ResumeGame()
+        {
+            PauseTimeTick.SetActive(true);
+            Time.timeScale = 1f;
+        }
+
+    }
+}
