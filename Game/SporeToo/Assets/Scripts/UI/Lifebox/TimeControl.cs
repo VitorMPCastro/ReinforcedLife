6541d16b4b4d0e6f6defb84777f913238126683b
Gabriel Rios <gabrielrios763@gmail.com>
2025-06-18 09:02:30 -0300

    TimeSpeedControl

---
+++ Game/SporeToo/Assets/Scripts/UI/Lifebox/TimeControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UI.Lifebox
+{
+    public class TimeControl : MonoBehaviour
+    {
+        public void Velocidade2x()
+        {
+            Time.timeScale = 2f;
+        }
+
+        public void Velocidade1x()
+        {
+            Time.timeScale = 1f;
+        }
+
+        public void VelocidadeLenta()
+        {
+            Time.timeScale = 0.5f;
+        }
+        
+    }
+}
