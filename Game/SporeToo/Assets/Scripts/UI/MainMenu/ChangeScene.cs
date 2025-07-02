fb31170f7917c3bd3aeea95a00a566975169a76e
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2025-07-01 22:23:01 -0300

    :art: style: Posicionamento dos botões na LifeBox

--- Game/SporeToo/Assets/Scripts/UI/MainMenu/ChangeScene.cs
+++ Game/SporeToo/Assets/Scripts/UI/MainMenu/ChangeScene.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-
     public void LoadIntroduction()
     {
         SceneManager.LoadScene("Introduction");
     }
+
     public void Playgame()
     {
         SceneManager.LoadScene("LifeBox");
     }
+
     public void BackMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
 }
+
